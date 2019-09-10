7f9afcdef793d74fe3f481863a2506daeb2a1c03
kevin <yafubark@comcast.net>
2019-09-09 22:49:38 -0500

    Beginning of BDD step implementations
    First two scenarios - error modals

--- features/search.feature
+++ features/search.feature
@@ -1,7 +1,6 @@
 Feature: searching on rocketmiles.com
 
 
-
 Scenario: Location is missing
     Given a web browser is at the rocketmiles home page
       And the location is blank
@@ -13,7 +12,7 @@ Scenario: Reward program is missing
     Given a web browser is at the rocketmiles home page
       And the reward program is blank
       And the location is not blank
-    When a search is intitiated`
+    When a search is initiated
     Then the missing reward program error is shown
 
 
--- features/steps/search.py
+++ features/steps/search.py
@@ -14,43 +14,54 @@ def step_impl(context, phrase):
     assert search_input.get_attribute('value') == phrase
 """
 
+import time
 from behave import * 
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 ROCKETMILES_HOME = 'https://www.rocketmiles.com'
+location_to_select = 'Peoria, IL, USA'
+
 
 @given('a web browser is at the rocketmiles home page')
 def step_impl(context):
     context.browser.get(ROCKETMILES_HOME)
+    time.sleep(20)
+    
 
 @given('the location is blank')
 def step_impl(context):
-    raise NotImplementedError('STEP: the location is blank')    
-    pass
+    location_dropdown = context.browser.find_element(By.NAME, 'locationSearch')
+    location_dropdown.clear()
+    selected_location = location_dropdown.text
+    assert(selected_location == '')
 
-@given('the location is not blank')
+
+@when('a search is initiated')
 def step_impl(context):
-    raise NotImplementedError('STEP: the location is not blank')    
+    context.browser.find_element(By.TAG_NAME, 'Button')
+
+
+@then('the missing location error is shown')
+def step_impl(context):
     pass
 
 @given('the reward program is blank')
 def step_impl(context):
-    raise NotImplementedError('STEP: the reward program is blank')    
-    pass
+    rewards_dropdown = context.browser.find_element(By.NAME, 'programAutosuggest')
+    rewards_dropdown.clear()
+    selected_program = rewards_dropdown.text
+    assert(selected_program == '')
 
-@When('a search is initiated')
-def step_impl(context):
-    raise NotImplementedError('STEP: When a search is intitiated')    
-    pass
 
-@then('the missing location error is shown')
+@given('the location is not blank')
 def step_impl(context):
-    raise NotImplementedError('STEP: the missing location error is shown')    
+    search_input = context.browser.find_element_by_name('q')
+    search_input.send_keys(phrase + Keys.RETURN)
     pass
 
 @then('the missing reward program error is shown')
 def step_impl(context):
-    raise NotImplementedError('STEP: the missing reward program error is shown')    
     pass
 
 @When('blank')
